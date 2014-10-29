4c48be8cdf3906bc1176efa16fdc174653da3183
hemasudheer <hsksudheer@gmail.com>
2014-10-29 15:24:10 +0000

    Update Test1.py

--- testscripts/Test1.py
+++ testscripts/Test1.py
@@ -4,11 +4,12 @@ Created on Oct 22, 2014
 @author: hemasudheer
 '''
 from pages.SFDriverScript import SFDriverScript
+from pages.SFLoginPage import SFLoginPage
 
 
 class CheckTest1(SFDriverScript):
 
-    def test_name1(self):
-        self.driver.get("https://login.salesforce.com/")
-        print self.driver.title
-        print "this is from my mbp"
+    def test_login_to_portal(self):
+        login = SFLoginPage(self.driver)
+        login.do_login()
+#         assert login.is_logged_in()
