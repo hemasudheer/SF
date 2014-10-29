d25f5ce40989914585ec101c7ccbf7a6f39d9010
hemasudheer <hsksudheer@gmail.com>
2014-10-29 15:23:32 +0000

    Update SFLoginPage.py

--- pages/SFLoginPage.py
+++ pages/SFLoginPage.py
@@ -3,16 +3,26 @@ Created on Oct 22, 2014
 
 @author: hemasudheer
 '''
+from pages import SFData
 from pages.SFBasePage import SFBasePage
-locators = {"username": "xpath=//input[@id='username']",
-            "password": "xpath=//input[@id='password']",
-            "login_button": "xpath=/*[@id='Login']",
+import time
+
+locators = {"username": "//input[@id='username']",
+            "password": "//*[@id='password']",
+            "login_button": "//*[@id='Login']",
+            "home_tab": "//*[@id='home_Tab']",
             }
 
 
 class SFLoginPage(SFBasePage):
 
     def do_login(self):
-        self.type(locators("username"), "hsksudheer@gmail.com")
-        self.type(locators("username"), "hsksudheer@gmail.com")
+        self.get_url(SFData.loginurl)
+        time.sleep(5)
+        self.type(locators["username"], SFData.credentials["username"])
+        self.type(locators["password"], SFData.credentials["password"])
+        self.press(locators["login_button"])
 
+    def is_logged_in(self):
+        self.wait_in_seconds(10)
+        self.is_element_present(locators["home_tab"])
